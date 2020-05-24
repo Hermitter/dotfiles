dbf73e5f70e81a92f08b4aa4d6c9b3bbad64cd21
Hermitter <chacin.carlos9798@gmail.com>
2020-05-24 15:14:46 -0400

    minor case change

--- scripts/fedora_installs.sh
+++ scripts/fedora_installs.sh
@@ -25,7 +25,7 @@ git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Install themes
+# install themes
 sudo dnf install -y flat-remix-icon-theme
 sudo dnf install -y materia-gtk-theme
 sudo dnf install -y gnome-shell-extension-material-shell
