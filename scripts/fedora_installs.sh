39d005d374c9c027597cb8cb9e5f9fb9340f15d5
Hermitter <chacin.carlos9798@gmail.com>
2020-06-07 21:34:14 -0400

    add wireshark

--- scripts/fedora_installs.sh
+++ scripts/fedora_installs.sh
@@ -1,29 +1,33 @@
 ## Right now this is more of a checklist than an actual script
 
-# first time update
+# First time update
 sudo dnf update -y 
 
-# enable rpm fusion's free&non-free repos
+# Enable rpm fusion's free&non-free repos
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# install essentials
-sudo dnf install -y fira-code-fonts lutris gnome-tweaks zsh toolbox tilix go ffmpeg steam SDL2-devel ssl-devel openocd ncurses-compat-libs glib glib-devel gtk-devel
+# Install essentials
+sudo dnf install -y fira-code-fonts lutris gnome-tweaks zsh toolbox tilix go ffmpeg steam SDL2-devel ssl-devel openocd ncurses-compat-libs glib glib-devel gtk-devel wireshark
 sudo groupinstall -y "Development Tools"
 sudo dnf install -y texlive-scheme-full
 
-# change shell to zsh
+# Change shell to zsh
 chsh -s /bin/zsh
 
-# download oh-my-zsh
+# Download oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# download fish-like plugins
+# Download zsh fish-like plugins
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# install themes
+# Install themes
 sudo dnf install -y flat-remix-icon-theme materia-gtk-theme gnome-shell-extension-material-shell roboto-fontface-fonts
 
-# increase number of file watchers
+# Increase number of file watchers
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+# Set up Wireshark (logout required to take effect)
+sudo usermod -a -G wireshark $USER
+sudo chmod +x /usr/bin/dumpcap # permissions fix
