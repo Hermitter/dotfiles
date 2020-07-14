7ea2d02bc351d6b8e241509755801ec923a1891c
Hermitter <chacin.carlos9798@gmail.com>
2020-07-13 22:37:24 -0400

    Set the desktop theme in Fedora install script

--- scripts/fedora_installs.sh
+++ scripts/fedora_installs.sh
@@ -60,6 +60,10 @@ sudo dnf install -y fira-code-fonts roboto-fontface-fonts
 # Install GNOME extentions
 sudo dnf install -y gnome-shell-extension-dash-to-dock gnome-shell-extension-appindicator.noarch gnome-shell-extension-user-theme
 
+# Change theme settings
+gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark-compact"
+gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Blue-Dark"
+
 #############################################
 # MISC
 #############################################
