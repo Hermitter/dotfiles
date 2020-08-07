3aecceb4e81133a6489133e44914a860429fb418
Hermitter <chacin.carlos9798@gmail.com>
2020-08-07 02:53:30 -0400

    Create script to add user in digital ocean droplet

---
+++ scripts/platforms/digital-ocean/add-user.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+read -p "Enter Username: " username
+read -p "Grant Sudo Privileges? (Y/N): " giveSudo && [[ $giveSudo == [yY] || $giveSudo == [yY][eE][sS] ]] || unset giveSudo
+
+# confirm creation
+echo -e "\n*************************"
+echo -e "Username: $username\nSudo Privileges: $giveSudo"
+echo -e "*************************\n"
+read -p "Create This User? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+#################
+## CREATE USER ##
+#################
+adduser $username
+if ! [ -z "$giveSudo" ]; then
+    usermod -aG sudo $username
+fi
+
+# enable external access (ssh key)
+rsync --archive --chown=$username:$username ~/.ssh /home/$username
+
+# enable firewall
+ufw allow OpenSSH
+ufw enable
