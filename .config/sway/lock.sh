be5b3ea8dc450bfc57000ad92e8475b35554a828
Hermitter <chacin.carlos9798@gmail.com>
2020-08-18 19:11:42 -0400

    Add image in lockscreen

--- .config/sway/lock.sh
+++ .config/sway/lock.sh
@@ -4,5 +4,4 @@ config="$HOME/.config/sway"
 background="$HOME/Pictures/Wallpapers/default.jpg"
 
 # swaylock --clock --datestr '%d.%m.%Y' --indicator -ef -i $background
-# image is not currently used
-swaylock -ef -i $HOME/Pictures/Wallpapers/default.jpg
+swaylock -e -f --image $background
