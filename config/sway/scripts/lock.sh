dcc6b1659230c79582a38bc9fc9229659d0e9563
Hermitter <chacin.carlos9798@gmail.com>
2020-08-21 16:28:33 -0400

    Replace lock.sh with swaylock command

--- config/sway/scripts/lock.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-config="$HOME/.config/sway"
-background="$HOME/Pictures/Wallpapers/default.jpg"
-
-# swaylock --clock --datestr '%d.%m.%Y' --indicator -ef -i $background
-swaylock -e -f --image $background
