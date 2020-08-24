7f5712d0d9d8700035a279975d1cfb0fdd65f8df
Hermitter <chacin.carlos9798@gmail.com>
2020-08-24 05:21:04 -0400

    Open waybar modules in a floating window

---
+++ config/sway/scripts/open_floating.sh
@@ -0,0 +1,18 @@
+# I love you: https://github.com/Synthetica9/sway-floating
+
+$@ &
+pid=$!
+
+swaymsg -t subscribe -m '[ "window" ]' \
+  | jq --unbuffered --argjson pid "$pid" '.container | select(.pid == $pid) | .id' \
+  | xargs -I '@' -- swaymsg '[ con_id=@ ] floating enable' &
+
+subscription=$!
+
+echo Going into wait state
+
+# Wait for our process to close
+tail --pid=$pid -f /dev/null
+
+echo Killing subscription
+kill $subscription
