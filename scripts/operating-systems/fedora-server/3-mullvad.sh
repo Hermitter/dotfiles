e36c3a7ef1f063cdd05deca40f409b5d7a3b3e8c
Carlos Chacin <chacin.carlos9798@gmail.com>
2020-10-05 13:56:07 -0400

    Mention that Mullvad installation is manual

--- scripts/operating-systems/fedora-server/3-mullvad.sh
+++ scripts/operating-systems/fedora-server/3-mullvad.sh
@@ -1,14 +1,12 @@
 #############################################
-# INSTALLATION 
+# INSTALLATION (NOT AUTOMATED)
 #############################################
-wget --content-disposition https://mullvad.net/download/rpm/latest
-sudo dnf -y install ./MullvadVPN-*
-
-# Remember to download the signature and check the package: (https://mullvad.net/en/download/#linux)
-# $ rpm --checksig MullvadVPN-XXXX_x86_64.rpm
+# Download RPM and GPG signature https://mullvad.net/en/download/#linux
+# rpm --checksig MullvadVPN-XXXX_x86_64.rpm
+# sudo dnf -y install ./MullvadVPN-XXXX_x86_64.rpm
 
 #############################################
-# CONFIG 
+# ACCOUNT/CONFIG SETUP
 #############################################
 read -p "Enter Mullvad Account Token: " MULLVAD_TOKEN
 mullvad account set $MULLVAD_TOKEN
