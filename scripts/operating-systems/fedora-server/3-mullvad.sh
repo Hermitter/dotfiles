85b89b29bb358eadda21400a44873145753bf309
Hermitter <chacin.carlos9798@gmail.com>
2020-10-05 11:39:56 -0400

    Use wild card for installing Mullvad rpm

--- scripts/operating-systems/fedora-server/3-mullvad.sh
+++ scripts/operating-systems/fedora-server/3-mullvad.sh
@@ -2,10 +2,10 @@
 # INSTALLATION 
 #############################################
 wget --content-disposition https://mullvad.net/download/rpm/latest
-# Remember to download the signature and check the package: (https://mullvad.net/en/download/#linux)
-# $ rpm --checksig MullvadVPN-2020.5_x86_64.rpm
+sudo dnf -y install ./MullvadVPN-*
 
-sudo dnf -y install ./MullvadVPN-XXXX.X_x86_64.rpm
+# Remember to download the signature and check the package: (https://mullvad.net/en/download/#linux)
+# $ rpm --checksig MullvadVPN-XXXX_x86_64.rpm
 
 #############################################
 # CONFIG 
