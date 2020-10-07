f269fa0d3b96926664b6d9832270919ce22583ff
Hermitter <chacin.carlos9798@gmail.com>
2020-10-07 09:03:59 -0400

    Fix zshrc cp typo in Fedora Server

--- scripts/operating-systems/fedora-server/1-install.sh
+++ scripts/operating-systems/fedora-server/1-install.sh
@@ -6,9 +6,6 @@ DOTFILES=../../..
 # APPLICATIONS / DEPENDENCIES 
 #############################################
 
-# Get config files
-cp $DOTFILES/zsh/*zshrc $HOME/.zshrc
-
 # Get personal scripts
 cp -r $DOTFILES/bin/ $HOME/.bin
 
@@ -43,6 +40,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Get config files
+cp $DOTFILES/zsh/zshrc $HOME/.zshrc
 
 # Create config files
 touch $HOME/.profile
