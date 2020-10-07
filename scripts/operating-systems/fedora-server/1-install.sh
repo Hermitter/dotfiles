85067dc5848aab244e91329e8d6f791c7495c811
Hermitter <chacin.carlos9798@gmail.com>
2020-10-07 09:20:36 -0400

    Add back ohmyzsh for Fedora server with exit notice

--- scripts/operating-systems/fedora-server/1-install.sh
+++ scripts/operating-systems/fedora-server/1-install.sh
@@ -34,12 +34,15 @@ cp $DOTFILES/zsh/zshrc $HOME/.zshrc
 chsh -s /bin/zsh
 
 # Download oh-my-zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "***************************"
+echo "CONTINUE THE INSTALLATION BY TYPING $ exit"
+echo "***************************"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Download zsh fish-like plugins
-# git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Create config files
 touch $HOME/.profile
