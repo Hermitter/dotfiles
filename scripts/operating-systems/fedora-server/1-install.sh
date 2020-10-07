289b6fd8d9d79e6af6cfad61b1d776605b3da268
Hermitter <chacin.carlos9798@gmail.com>
2020-10-07 09:17:03 -0400

    Fix git clone for zsh autosuggestions

--- scripts/operating-systems/fedora-server/1-install.sh
+++ scripts/operating-systems/fedora-server/1-install.sh
@@ -37,7 +37,7 @@ chsh -s /bin/zsh
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Download zsh fish-like plugins
-# git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
