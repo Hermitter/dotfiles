31e32448781e848bdc6d7f07e713ea1393c01471
Hermitter <chacin.carlos9798@gmail.com>
2020-10-07 09:13:00 -0400

    Comment our ohmyzsh installation in Fedora Server

--- scripts/operating-systems/fedora-server/1-install.sh
+++ scripts/operating-systems/fedora-server/1-install.sh
@@ -34,15 +34,12 @@ cp $DOTFILES/zsh/zshrc $HOME/.zshrc
 chsh -s /bin/zsh
 
 # Download oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Download zsh fish-like plugins
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Get config files
-cp $DOTFILES/zsh/zshrc $HOME/.zshrc
+# git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Create config files
 touch $HOME/.profile
