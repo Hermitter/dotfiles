a70224b52bc178f7faf4e1fa7fe4e4e3d71d2484
Carlos Chacin <chacin.carlos9798@gmail.com>
2020-11-22 19:52:14 -0500

    Remove white space from empty new lines

--- config/waybar/modules/spotify.sh
+++ config/waybar/modules/spotify.sh
@@ -4,15 +4,15 @@ icon=""
 
 if [[ $class == "playing" ]]; then
   info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
-  
+
   if [[ ${#info} -eq 3 ]]; then
     info=""
   elif [[ ${#info} -gt 30 ]]; then
     info=$(echo $info | cut -c1-30)"..."
   fi
-  
+
   text=$icon" "$info
-  
+
 elif [[ $class == "paused" ]]; then
   text=$icon" PAUSED"
 elif [[ $class == "stopped" ]]; then
