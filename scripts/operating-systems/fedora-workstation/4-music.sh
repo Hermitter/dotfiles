e819434bca83aaf2919227c6c6ed7f607cc653d5
Hermitter <chacin.carlos9798@gmail.com>
2021-05-14 07:54:36 -0400

    Use pipewire jack on Fedora Workstation

--- scripts/operating-systems/fedora-workstation/4-music.sh
+++ scripts/operating-systems/fedora-workstation/4-music.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-#############################################
-# APPLICATIONS 
-#############################################
-sudo dnf install -y \
-qsynth amsynth \
-ardour5 
-
-flatpak install -y net.sourceforge.VMPK
+#TODO: test this on a new machine
 
 #############################################
 # JACK SOUND SERVER 
 #############################################
-sudo dnf install -y \
-jack-audio-connection-kit-dbus \
-jack-audio-connection-kit-example-clients \
-qjackctl
+sudo dnf install -y pipewire-jack-audio-connection-kit
 
 # Enable permissions for user (logout/login to apply changes)
-sudo usermod -a -G jackuser $USER
+# TODO: check if this is still needed
+# sudo usermod -a -G jackuser $USER
+
+#############################################
+# APPLICATIONS 
+#############################################
+sudo dnf install -y \
+qjackctl \
+qsynth \
+ardour5 
