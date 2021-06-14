6855cef2ed8dc1283095c83382ce5fae3d66ab10
Hermitter <chacin.carlos9798@gmail.com>
2021-06-14 02:01:32 -0400

    Add untested Fedora Silverblue scripts

---
+++ scripts/operating-systems/fedora-silverblue/1-install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+DOTFILES="$(dirname $(dirname $(dirname $(dirname "$( realpath "${BASH_SOURCE[0]}")"))))"
+
+# Increase DNF speeds
+sudo bash -c "echo -e 'max_parallel_downloads=10\nfastestmirror=True' >> /etc/dnf/dnf.conf"
+
+rpm-ostree upgrade
+
+# Get config files
+cp -r $DOTFILES/config/* $HOME/.config
+
+# Get personal scripts
+BIN=$HOME/.bin
+cp -r $DOTFILES/bin/ $BIN
+curl -L https://github.com/Hermitter/tepe/releases/latest/download/tepe-linux-amd64 -o $BIN/tepe && chmod +x $BIN/tepe
+
+# Get wallpapers
+cp -r $DOTFILES/images/Wallpapers $HOME/Pictures/Wallpapers
+
+#############################################
+# APPLICATIONS / DEPENDENCIES 
+#############################################
+
+# Enable rpm fusion's free&non-free repos
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Enable flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install essentials
+rpm-ostree install \
+tilix \
+podman-compose \
+util-linux-user \
+zsh exa starship
+
+# Install Fedora Flatpak apps
+flatpak install fedora -y \
+org.gnome.Geary \
+org.gnome.Extensions \
+com.transmissionbt.Transmission
+
+# Install Flathub Flatpak apps
+flatpak install flathub -y \
+org.wireshark.Wireshark \
+org.pulseaudio.pavucontrol \
+com.valvesoftware.Steam \
+com.spotify.Client \
+org.telegram.desktop \
+org.signal.Signal \
+com.discordapp.Discord \
+com.slack.Slack \
+org.gabmus.hydrapaper \
+com.dropbox.Client \
+md.obsidian.Obsidian
+
+# Generate ssh keys
+ssh-keygen
+
+# Create config files
+touch $HOME/.profile
+touch $HOME/.secrets
+
+#############################################
+# THEME
+#############################################
+
+# Install GTK theme and flatpak theme
+rpm-ostree install flat-remix-icon-theme materia-gtk-theme
+flatpak install -y flathub org.gtk.Gtk3theme.Materia{,-dark,-light}{,-compact}
+
+# Install Fonts
+rpm-ostree install fira-code-fonts roboto-fontface-fonts
+
+# Install GNOME extentions
+rpm-ostree install gnome-shell-extension-appindicator
+
+#############################################
+# MISC
+#############################################
+
+# git configs
+git config pull.rebase false --global
+
+# Increase number of file watchers
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+#############################################
+# WRAPPING UP
+#############################################
+
+echo -e '\nFINISHED INSTALLING: 1-install.sh\n~~~~~~~~~~ Please Reboot ~~~~~~~~~~\n'
+
+#############################################
+# UNUSED AREA
+#############################################
+# Firefox (about:config) these settings can't be automated >:(
+# gfx.webrender.all
+# layers.acceleration.force-enabled
---
+++ scripts/operating-systems/fedora-silverblue/2-install.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Install media codecs
+# TODO: find more: gstreamer1-plugin-openh264
+rpm-ostree install ffmpeg
+
+# Unsure if needed in flatpak version
+# # Set up Wireshark
+# sudo usermod -a -G wireshark $USER
+# sudo chmod +x /usr/bin/dumpcap # permissions fix
+
+#############################################
+# FLATPAK THEME WORKAROUND
+#############################################
+
+# Here we're going to copy the system materia theme and place it in the user
+# themes folder due to flatpak is having issues detecting custom themes.
+# (not ideal, but it works)
+mkdir -p ~/.themes
+SYS_THEMES=/usr/share/themes
+USR_THEMES=~/.themes
+
+cp -r $SYS_THEMES/Materia $USR_THEMES/Materia
+cp -r $SYS_THEMES/Materia-compact $USR_THEMES/Materia-compact
+cp -r $SYS_THEMES/Materia-dark $USR_THEMES/Materia-dark
+cp -r $SYS_THEMES/Materia-dark-compact $USR_THEMES/Materia-dark-compact
+cp -r $SYS_THEMES/Materia-light $USR_THEMES/Materia-light
+cp -r $SYS_THEMES/Materia-light-compact $USR_THEMES/Materia-light-compact
+
+# Force Flatpaks to use user installed themes 
+sudo flatpak override --filesystem=~/.themes
+
+#############################################
+# SHELL
+#############################################
+# Download oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Download zsh fish-like plugins
+git clone git://github.com/hermitter/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/hermitter/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/hermitter/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Get zsh config
+cp $DOTFILES/zsh/zshrc $HOME/.zshrc
+
+#############################################
+# WRAPPING UP
+#############################################
+
+echo -e '\nFINISHED INSTALLING: 2-install.sh\n~~~~~~~~~~ Please Reboot ~~~~~~~~~~\n'
+
+# Start zsh session
+exec zsh -l
+
+# Change shell to zsh
+echo "Changing shell to zsh..."
+chsh -s /bin/zsh
