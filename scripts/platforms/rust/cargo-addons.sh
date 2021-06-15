c139a093e2321fab1c696050efc4946fc5e8b7c7
Hermitter <chacin.carlos9798@gmail.com>
2021-06-15 01:32:50 -0400

    Add script to install helpful cargo tools

---
+++ scripts/platforms/rust/cargo-addons.sh
@@ -0,0 +1,4 @@
+# Download rust from: https://www.rust-lang.org/tools/install
+cargo install --version=0.7.0 cargo-edit
+cargo install --version=0.14.1 cargo-audit
+cargo install --locked --version=0.5.5 sqlx-cli --no-default-features --features postgres
