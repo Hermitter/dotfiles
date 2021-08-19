b9266d5b9fafffa335161497820bff6dbbe8ac6f
Hermitter <chacin.carlos9798@gmail.com>
2021-08-19 03:56:41 -0400

    Remove extra line break

--- scripts/operating-systems/centos-server/3-docker.sh
+++ scripts/operating-systems/centos-server/3-docker.sh
@@ -9,7 +9,6 @@ sudo systemctl enable --now docker
 # Add user to docker group (applies on reboot)
 sudo usermod -aG docker $USER
 
-
 # Install docker-compose (https://docs.docker.com/compose/install/)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
