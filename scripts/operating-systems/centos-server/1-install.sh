5d3f315ee61ebd96208532b64e4feda875ef2334
Hermitter <chacin.carlos9798@gmail.com>
2021-08-19 03:02:11 -0400

    Rename CentOS dir and add docker script

--- scripts/operating-systems/centos-server/1-install.sh
+++ scripts/operating-systems/centos-server/1-install.sh
@@ -27,18 +27,6 @@ trash-cli
 # Enable Cockpit web dashboard (localhost:9090)
 sudo systemctl enable --now cockpit.socket
 
-# TODO: seperate docker into seperate file
-# Install Docker
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io --allowerasing
-sudo usermod -aG docker $USER
-sudo systemctl enable --now docker
-
-# Install docker-compose (https://docs.docker.com/compose/install/)
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 # Install wireguard
 sudo dnf install -y kmod-wireguard wireguard-tools
 # sudo modprobe wireguard (enables wireguard until reboot)
---
+++ scripts/operating-systems/centos-server/3-docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install Docker (https://docs.docker.com/engine/install/centos/)
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io --allowerasing
+sudo usermod -aG docker $USER
+sudo systemctl enable --now docker
+
+# Install docker-compose (https://docs.docker.com/compose/install/)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
