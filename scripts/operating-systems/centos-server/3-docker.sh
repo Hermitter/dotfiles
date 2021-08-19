1b8af4de75364eee5331d9292a32ddf53db505e6
Hermitter <chacin.carlos9798@gmail.com>
2021-08-19 03:56:11 -0400

    Add missing auto confirm in CentOS script

--- scripts/operating-systems/centos-server/3-docker.sh
+++ scripts/operating-systems/centos-server/3-docker.sh
@@ -3,9 +3,12 @@
 # Install Docker (https://docs.docker.com/engine/install/centos/)
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io --allowerasing
-sudo usermod -aG docker $USER
+sudo dnf install -y docker-ce docker-ce-cli containerd.io --allowerasing
 sudo systemctl enable --now docker
+
+# Add user to docker group (applies on reboot)
+sudo usermod -aG docker $USER
+
 
 # Install docker-compose (https://docs.docker.com/compose/install/)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
