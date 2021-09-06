9090badff7f8e196ba11e93149208d37c93cb2d0
Hermitter <chacin.carlos9798@gmail.com>
2021-09-06 00:05:04 -0400

    Add Aruino setup for Silverblue

---
+++ scripts/operating-systems/fedora-silverblue/6-arduino.sh
@@ -0,0 +1,10 @@
+# Allow user to access serial ports
+GROUP=dialout
+sudo bash -c "grep -E '^$GROUP:' /usr/lib/group >> /etc/group"
+sudo usermod -aG $GROUP $USER
+
+# Install Adruino IDE
+flatpak install -y flathub cc.arduino.arduinoide
+
+# Reboot to load group changes to user
+echo -e '\nFINISHED INSTALLING: 6-arduino.sh\n~~~~~~~~~~ Please Reboot ~~~~~~~~~~\n'
