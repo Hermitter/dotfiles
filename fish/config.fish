163bbc40e094a7ee0a7fb8dc06db75149666afc4
Hermitter <chacin.carlos9798@gmail.com>
2022-02-16 16:45:23 -0500

    Add aliases in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,16 @@
 # Fix unthemed ls output: https://linuxhint.com/ls_colors_bash/
 set -Ux LS_COLORS 'di=1a;35:fi=0;0'
 
+# https://containertoolbx.org/
+if exists toolbox
+    alias tb 'toolbox'
+
+    # Expose scripts meant for use in toolbox
+    if set -q TOOLBOX_PATH
+        fish_add_path ~/.toolbox_bin
+    end
+end
+
 # https://starship.rs
 if exists starship
     starship init fish | source
@@ -9,4 +19,9 @@ end
 # https://the.exa.website
 if exists exa
     alias ls 'exa'
+end
+
+# https://www.terraform.io/
+if exists terraform
+    alias tf 'terraform'
 end
