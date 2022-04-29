9efe2168065342871bb57166a752ecf371c57c3a
Hermitter <chacin.carlos9798@gmail.com>
2022-04-29 16:40:53 -0400

    Cleanup toolbox scripts

--- scripts/operating-systems/fedora-silverblue/toolboxes/lutris.sh
+++ scripts/operating-systems/fedora-silverblue/toolboxes/lutris.sh
@@ -1,67 +1,42 @@
 #!/usr/bin/env bash
-TOOLBOX=lutris-toolbox
-toolbox create --container $TOOLBOX
+TOOLBOX_NAME=lutris-toolbox
+toolbox create --container $TOOLBOX_NAME
+TB_RUN="toolbox run --container $TOOLBOX_NAME $@"
 
 # Increase download speeds
-toolbox run --container $TOOLBOX sudo bash -c "echo -e 'max_parallel_downloads=20\nfastestmirror=True' >> /etc/dnf/dnf.conf"
+$TB_RUN sudo bash -c "echo -e 'max_parallel_downloads=20\nfastestmirror=True' >> /etc/dnf/dnf.conf"
 
 #############################################
 # APPLICATIONS / DEPENDENCIES 
 #############################################
-toolbox run --container $TOOLBOX sudo dnf install -y \
+$TB_RUN sudo dnf install -y \
 lutris \
 wine
 
+# Set locale
+$TB_RUN sudo bash -c 'echo -e "export LC_ALL=C.UTF-8\n" >> /etc/bashrc'
+
 #############################################
-# LINK HOST'S FIREFOX
+# Expose host tools to toolbox 
 #############################################
-# Set host's firefox as the default browser
-# This is so toolbox apps (mainly VScode) can open the host's browser
-toolbox run --container $TOOLBOX sudo dnf install xdg-utils -y
-HOST_FIREFOX="[Desktop Entry]
-Version=1.0
-Name=Firefox
-Exec=flatpak-spawn --host firefox %u
-Icon=firefox
-Terminal=false
-Type=Application
-MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;text/mml;x-scheme-handler/http;x-scheme-handler/https;
-StartupNotify=true
-Actions=new-window;new-private-window;profile-manager-window;
-
-X-Desktop-File-Install-Version=0.26
-
-[Desktop Action new-window]
-Name=Open a New Window
-Exec=flatpak-spawn --host firefox --new-window %u
-
-[Desktop Action new-private-window]
-Name=Open a New Private Window
-Exec=flatpak-spawn --host firefox --private-window %u
-
-[Desktop Action profile-manager-window]
-Name=Open the Profile Manager
-Exec=flatpak-spawn --host firefox --ProfileManager
-"
-toolbox run --container $TOOLBOX bash -c "echo '$HOST_FIREFOX' | sudo tee /usr/share/applications/toolbox.host.firefox.desktop"
-toolbox run --container $TOOLBOX xdg-settings set default-web-browser toolbox.host.firefox.desktop
+$TB_RUN sudo bash -c 'echo "#\!/usr/bin/env bash
+  flatpak-spawn --host /usr/bin/xdg-open "\$@"
+" > /usr/local/bin/xdg-open'
+$TB_RUN sudo chmod +x /usr/local/bin/xdg-open
 
 #############################################
 # LUTRIS DESKTOP FILE
 #############################################
-# Add .desktop file to toolbox's VScode
-LUTRIS_DESKTOP="[Desktop Entry]
-Name=Lutris
-Comment=video game preservation platform
-Categories=Game;
-Keywords=gaming;wine;emulator;
-Exec=toolbox run --container $TOOLBOX lutris %U
-Icon=lutris
-Terminal=false
-Type=Application
-MimeType=x-scheme-handler/lutris;
-"
-echo "$LUTRIS_DESKTOP" > ~/.local/share/applications/net.lutris.Lutris.desktop
+# Create .desktop files to open toolbox GUI apps from host
+function create_host_desktop_file {
+    app_name=$1
+    toolbox_destop_file=/usr/share/applications/$app_name.desktop
+    host_desktop_file=~/.local/share/applications/$app_name.desktop
+
+    $TB_RUN sed "s/Exec=/Exec=toolbox run --container "$TOOLBOX_NAME" /g" $toolbox_destop_file > $host_desktop_file
+}
+
+create_host_desktop_file "net.lutris.Lutris"
 
 # Set Lutris app to open lutris links
 xdg-settings set default-url-scheme-handler lutris net.lutris.Lutris.desktop
--- scripts/operating-systems/fedora-silverblue/toolboxes/primary.sh
+++ scripts/operating-systems/fedora-silverblue/toolboxes/primary.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+TB_RUN="toolbox run $@"
 toolbox create -y
 
 # Increase DNF download speeds
-toolbox run sudo bash -c "echo -e 'max_parallel_downloads=20\nfastestmirror=True' >> /etc/dnf/dnf.conf"
+$TB_RUN sudo bash -c "echo -e 'max_parallel_downloads=20\nfastestmirror=True' >> /etc/dnf/dnf.conf"
 
 #############################################
 # APPLICATIONS / DEPENDENCIES 
 #############################################
-toolbox run sudo dnf install -y \
+$TB_RUN sudo dnf install -y \
 zsh \
 fish \
 starship \
@@ -21,47 +22,47 @@ net-tools \
 nghttp2 \
 usbutils \
 
-toolbox run sudo dnf groupinstall -y "Development Tools"
+$TB_RUN sudo dnf groupinstall -y "Development Tools"
 
 # Set locale
-toolbox run sudo bash -c 'echo -e "export LC_ALL=C.UTF-8\n" >> /etc/zshrc'
-toolbox run sudo bash -c 'echo -e "export LC_ALL=C.UTF-8\n" >> /etc/bashrc'
-toolbox run sudo bash -c 'echo -e "set -x LC_ALL C.UTF-8\n" >> /etc/fish/config.fish'
+$TB_RUN sudo bash -c 'echo -e "export LC_ALL=C.UTF-8\n" >> /etc/zshrc'
+$TB_RUN sudo bash -c 'echo -e "export LC_ALL=C.UTF-8\n" >> /etc/bashrc'
+$TB_RUN sudo bash -c 'echo -e "set -x LC_ALL C.UTF-8\n" >> /etc/fish/config.fish'
 
 #############################################
 # Expose host tools to toolbox 
 #############################################
 # podman
-toolbox run sudo bash -c 'echo "#\!/usr/bin/env bash
+$TB_RUN sudo bash -c 'echo "#\!/usr/bin/env bash
   flatpak-spawn --host /usr/bin/podman "\$@"
 " > /usr/local/bin/podman'
-toolbox run sudo chmod +x /usr/local/bin/podman
+$TB_RUN sudo chmod +x /usr/local/bin/podman
 
 # podman-compose
-toolbox run sudo bash -c 'echo "#\!/usr/bin/env bash
+$TB_RUN sudo bash -c 'echo "#\!/usr/bin/env bash
   flatpak-spawn --host /usr/bin/podman-compose "\$@"
 " > /usr/local/bin/podman-compose'
-toolbox run sudo chmod +x /usr/local/bin/podman-compose
+$TB_RUN sudo chmod +x /usr/local/bin/podman-compose
 
 # xdg-open (Opens all URLs in the host)
-toolbox run sudo bash -c 'echo "#\!/usr/bin/env bash
+$TB_RUN sudo bash -c 'echo "#\!/usr/bin/env bash
   flatpak-spawn --host /usr/bin/xdg-open "\$@"
 " > /usr/local/bin/xdg-open'
-toolbox run sudo chmod +x /usr/local/bin/xdg-open
+$TB_RUN sudo chmod +x /usr/local/bin/xdg-open
 
 #############################################
 # VS Code
 #############################################
 # Source: https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
-toolbox run sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-toolbox run sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-toolbox run sudo dnf check-update
-toolbox run sudo dnf install code -y
+$TB_RUN sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+$TB_RUN sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+$TB_RUN sudo dnf check-update
+$TB_RUN sudo dnf install code -y
 
 # Add missing VScode dependencies. This fixes: 
 # - "error while loading shared libraries: libxshmfence.so.1"
 # - missing emojis
-toolbox run sudo dnf install -y \
+$TB_RUN sudo dnf install -y \
 qt5-qtwayland \
 gdouros-symbola-fonts
 
@@ -71,7 +72,7 @@ function create_host_desktop_file {
     toolbox_destop_file=/usr/share/applications/$app_name.desktop
     host_desktop_file=~/.local/share/applications/$app_name.desktop
 
-    toolbox run sed 's/Exec=/Exec=toolbox run /g' $toolbox_destop_file > $host_desktop_file
+    $TB_RUN sed "s/Exec=/Exec=toolbox run /g" $toolbox_destop_file > $host_desktop_file
 }
 
 create_host_desktop_file "code"
