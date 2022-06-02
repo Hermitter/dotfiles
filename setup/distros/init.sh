4b7c3d08e1b8da5d49c19e688fd913dc2b800d64
Carlos Chacin <Hermitter@jellyapple.com>
2022-06-02 02:48:10 -0400

    Begin work on silverblue setup script

---
+++ setup/distros/init.sh
@@ -0,0 +1,32 @@
+readonly LINUX_SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
+
+
+################################################################################
+# Distro
+#
+# Detect/Run the setup script for the current Linux distribution. 
+################################################################################
+
+# TODO: add more cases for distros not following https://www.man7.org/linux/man-pages/man5/os-release.5.html
+if [ -f /etc/os-release ]; then
+    . "/etc/os-release"
+    OS=$ID
+    VARIANT=$VARIANT_ID
+else
+    log_fatal "Unable to detect Linux distro"
+fi
+
+if [[ $OS == 'fedora' ]] && [[ $VARIANT == 'silverblue' ]]; then
+    source "$LINUX_SCRIPT_DIR/silverblue.sh"
+else
+    log_status "Detected OS: $OS $VARIANT"
+    log_fatal "This distro has no setup scripts..."
+fi
+
+################################################################################
+# Desktop
+#
+# Detect/Run the setup script for the current Desktop environment. 
+################################################################################
+
+log_todo "Detect desktop enviorment and run its setup script"
---
+++ setup/distros/silverblue.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "YOU MADE IT TO THE SILVERBLUE INSTALL"
