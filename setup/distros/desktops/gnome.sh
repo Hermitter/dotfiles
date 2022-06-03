cb4e656bef9438d429a9b05da89cf3616c369e96
Carlos Chacin <Hermitter@jellyapple.com>
2022-06-03 12:50:59 -0400

    Escape each bash variables in autostart script

--- setup/distros/desktops/gnome.sh
+++ setup/distros/desktops/gnome.sh
@@ -171,23 +171,23 @@ gsettings set org.gnome.shell disable-user-extensions false
 gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
 
 function start_apps {
-    for pair in "$@"; do
+    for pair in "\$@"; do
         # split arg into seperate variables
-        IFS=:; set -- $pair
-        NAME=$1
-        CMD=$2
+        IFS=:; set -- \$pair
+        NAME=\$1
+        CMD=\$2
 
-        if pgrep -x "$NAME" > /dev/null
+        if pgrep -x "\$NAME" > /dev/null
         then
-            echo "Not starting '$NAME'. App already running"
+            echo "Not starting '\$NAME'. App already running"
         else
-            eval $CMD &
+            eval \$CMD &
         fi
     done
 
     # Kill each child process. The disown command was not used because an empty process was
     # left behind for each app that was disowned.
-    pkill -P $$
+    pkill -P \$\$
 }
 
 # process_name : run_command
