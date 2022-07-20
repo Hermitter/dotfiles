9b608263dac8aa96594a7b37835c6633d6ab464c
Carlos Chacin <Hermitter@jellyapple.com>
2022-07-20 11:32:28 -0400

    Upgrade to adw-gtk3 v3.4

--- setup/distros/desktops/gnome.sh
+++ setup/distros/desktops/gnome.sh
@@ -10,8 +10,8 @@ if ! [[ -d "$HOME/.themes/adw-gtk3" ]]; then
     log_status 'Installing Adwaita GTK theme'
 
     ADW_TMP=$(mktemp -d)
-    ADW_TAR='adw-gtk3v2-0.tar.xz'
-    wget -P "$ADW_TMP" "https://github.com/lassekongo83/adw-gtk3/releases/download/v2.0/$ADW_TAR"
+    ADW_TAR='adw-gtk3v3-4.tar.xz'
+    wget -P "$ADW_TMP" "https://github.com/lassekongo83/adw-gtk3/releases/download/v3.4/$ADW_TAR"
     tar -C "$HOME/.themes" -xvf "$ADW_TMP/$ADW_TAR" adw-gtk3 adw-gtk3-dark
 
     log_success 'Installed Adwaita GTK theme'
