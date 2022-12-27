ed08bd6c2e81622bc240adf5d120e12e3c1ea15e
Carlos Chacin <Hermitter@jellyapple.com>
2022-12-27 16:51:30 -0500

    Remove cargo-edit and upgrade remaining addons

--- setup/tools/rust/cargo-addons.sh
+++ setup/tools/rust/cargo-addons.sh
@@ -1,4 +1,4 @@
 # Download rust from: https://www.rust-lang.org/tools/install
-cargo install --version=0.7.0 cargo-edit
-cargo install --version=0.14.1 cargo-audit
-cargo install --locked --version=0.5.5 sqlx-cli --no-default-features --features postgres
+rustup default stable
+cargo install --version=0.17.4 cargo-audit
+cargo install --locked --version=0.6.2 sqlx-cli --no-default-features --features postgres
